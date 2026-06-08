f5cf8616f627c55f88ac29e82baa112c2f66f64f
BobRoss2525 <brouse303@gmail.com>
2026-06-08 15:25:04 -0500

    Test more

--- Bank.py
+++ Bank.py
@@ -1,28 +1,28 @@
-# # Simple Python Example
+# Simple Python Example
 
-# # Variables
-# name = "Alice"
-# age = 25
+# Variables
+name = "Alice"
+age = 25
 
-# # Print a greeting
-# print("Hello, " + name + "!")
-# print("You are", age, "years old.")
+# Print a greeting
+print("Hello, " + name + "!")
+print("You are", age, "years old.")
 
-# # Simple math
-# a = 10
-# b = 5
-# print("Sum:", a + b)
-# print("Difference:", a - b)
-# print("Product:", a * b)
-# print("Division:", a / b)
+# Simple math
+a = 10
+b = 5
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Division:", a / b)
 
-# # If statement
-# if age >= 18:
-#     print(name, "is an adult.")
-# else:
-#     print(name, "is a minor.")
+# If statement
+if age >= 18:
+    print(name, "is an adult.")
+else:
+    print(name, "is a minor.")
 
-# # Loop
-# print("Counting to 5:")
-# for i in range(1, 6):
-#     print(i)
+# Loop
+print("Counting to 5:")
+for i in range(1, 6):
+    print(i)
