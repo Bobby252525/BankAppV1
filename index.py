1e9f54bee65e5dabaad9d1f53f7f14e2990b41e9
BobRoss2525 <brouse303@gmail.com>
2026-06-08 15:01:26 -0500

    Infinite loop

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
     while(true) {
         print("Hello, World!");
     }
+    return """
     <h1>My App</h1>
     <form action="/greet" method="get">
         <input type="text" name="name" placeholder="Type your name...">
